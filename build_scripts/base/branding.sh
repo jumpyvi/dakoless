0751dbe66aee23e782f5781af10bfbb3f4becd15
JumpyVi <jumpyvi@outlook.com>
2025-12-25 20:10:54 -0500

    Simplify things

--- build_scripts/base/branding.sh
+++ build_scripts/base/branding.sh
@@ -5,7 +5,7 @@ set -xeuo pipefail
 IMAGE_REF="ostree-image-signed:docker://ghcr.io/${IMAGE_VENDOR}/${IMAGE_NAME}"
 IMAGE_INFO="/usr/share/ublue-os/image-info.json"
 IMAGE_FLAVOR="main"
-IMAGE_TAG="nightly"
+IMAGE_TAG="latest"
 
 cat >$IMAGE_INFO <<EOF
 {
@@ -13,11 +13,11 @@ cat >$IMAGE_INFO <<EOF
   "image-ref": "${IMAGE_REF}",
   "image-flavor": "${IMAGE_FLAVOR}",
   "image-vendor": "${IMAGE_VENDOR}",
-  "image-tag": "${IMAGE_TAG}",
+  "image-tag": "${IMAGE_TAG}"
 }
 EOF
 
-IMAGE_PRETTY_NAME="Bluefin Distroless"
+IMAGE_PRETTY_NAME="Bluefin"
 HOME_URL="https://projectbluefin.io"
 DOCUMENTATION_URL="https://docs.projectbluefin.io"
 SUPPORT_URL="https://github.com/projectbluefin/distroless/issues"
@@ -25,24 +25,16 @@ BUG_SUPPORT_URL="https://github.com/projectbluefin/distroless/issues"
 CODE_NAME="Dakotaraptor"
 ID="bluefin-distroless"
 
-# OS Release File (changed in order with upstream)
-sed -i -f - /usr/lib/os-release <<EOF
-    s/^NAME=.*/NAME="${IMAGE_PRETTY_NAME}"/
-    s/^ID=.*/ID="${ID}"/
-    s/^PRETTY_NAME=.*/PRETTY_NAME="${IMAGE_PRETTY_NAME}"/
-    s|^BUG_REPORT_URL=.*|BUG_REPORT_URL="${BUG_SUPPORT_URL}"|
-    s|^HOME_URL=.*|HOME_URL="${HOME_URL}"|
-    /^VERSION_CODENAME=/d
-    /^VERSION_ID=.*/a VERSION_CODENAME="${CODE_NAME}"
-    /^IMAGE_VERSION=/d
-    /^SYSEXT_LEVEL=/d
-    /^GNOMEOS_COMMIT=/d
-    /^GNOMEOS_COMMIT_DATE=/d
-    /^GNOMEOS_COMMITS_URL=/d
-EOF
-
-# Extra file content
-cat >> /usr/lib/os-release <<EOF
+# OS-Release
+cat > /usr/lib/os-release <<EOF
+NAME="${IMAGE_PRETTY_NAME}"
+ID="${ID}"
+ID_LIKE="org.gnome.os"
+VERSION="${IMAGE_TAG}"
+VERSION_CODENAME="${CODE_NAME}"
+PRETTY_NAME="${IMAGE_PRETTY_NAME}"
+BUG_REPORT_URL="${BUG_SUPPORT_URL}"
+HOME_URL="${HOME_URL}"
 DOCUMENTATION_URL="${DOCUMENTATION_URL}"
 SUPPORT_URL="${SUPPORT_URL}"
 LOGO=img-logo-icon
